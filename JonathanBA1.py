34ce69ac682ed345772ce5c0159bad6dc52225f8
JonathanBAlmeida <86905221+JonathanBAlmeida@users.noreply.github.com>
2021-07-04 08:47:42 -0700

    My first project

---
+++ JonathanBA1.py
@@ -0,0 +1,8 @@
+# programe to print name
+name = input("Enter your name : ")
+roll = int(input("Enter your Roll No : "))
+hoob = input("Enter your hoby : ")
+
+print = ('Name : ' , name)
+print = ('Roll No : ', roll)
+print = ('Hobby : ', hoob)
